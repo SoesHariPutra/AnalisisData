be4bf23f7f281e8172dd60e3625057ee80262960
Soes Hari Putra <38636474+soeshariputra@users.noreply.github.com>
2018-12-10 14:40:43 +0700

    Building block Apps

---
+++ acquire/twitter.py
@@ -0,0 +1,15 @@
+"""
+Mengambil data dari twitter
+"""
+def get_data():
+    dummy_data = [
+        {
+            'username': '@bpkri',
+            'last_tweet': '@CPNSBPK @bpkri ON PROCESS'
+        },
+        {
+            'username': '@jsnover',
+            'last_tweet': 'PowerShell'
+        }
+    ]
+    return dummy_data
