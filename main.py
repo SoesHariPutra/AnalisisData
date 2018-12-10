f755ca3b64d5d11797798f07c4efa6da6e1bea0d
Soes Hari Putra <soeshari.putra@outlook.com>
2018-12-10 14:42:26 +0700

    Building block Apps

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ Program untuk menganalisis data dari suatu sumber,
 diproses dan ditampilkan hasilnya
 """
 
-from acquire import twitter
-from process import decision_tree
-from views import text
+from acquire import twitter #, facebook, csv, xlsx, docx
+from process import decision_tree #, random,_forest
+from views import text #, image, chart
 
 data = twitter.get_data() #acquire
 result = decision_tree.process_data(data) #process
 display = text.as_text(result) #views
 
-print(result)
+print(display)
